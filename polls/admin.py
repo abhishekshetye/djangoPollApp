0eddd4169cb15a98c9327bca68e0a6d43900a2fc
abhishekshetye <abhishek.shetye@ves.ac.in>
2017-06-26 13:23:51 +0530

    add question and choices from admin site

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question
+from .models import Choice
+
+#admin.site.register(Question)
+#admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'question_text']
+    list_display = ('question_text', 'pub_date', 'recentQuestion')
+    list_filter = ['pub_date']
+    fieldsets = [
+        (None,          {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
 
 
-admin.site.register(Question)
+admin.site.register(Question,QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -8,6 +8,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    search_fields = ['question_text']
 
     def __str__(self):
         return self.question_text
@@ -16,6 +17,10 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
         #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    recentQuestion.admin_order_field = 'pub_date'
+    recentQuestion.boolean = True
+    recentQuestion.short_description = 'Published recently?'
+
 
 
 class Choice(models.Model):
